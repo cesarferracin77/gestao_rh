a4d3c047ba0c9e1ee8b198b19a47fcbbd4594ef7
Cesar Luiz Ferracin <cferracin@opencs.com.br>
2020-12-21 15:47:10 -0500

    Definindo as relações de foreing key entre os modelos

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
+
 
 # Create your models here.
 class Documento(models.Model):
     descricao = models.CharField(max_length=100, help_text='Descrição do documento')
+    dono = models.ForeignKey(
+        Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
