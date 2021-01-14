9fddf561c34246e4504370dec05f130c17a19558
Cesar Luiz Ferracin <cferracin@opencs.com.br>
2021-01-14 11:27:53 -0500

    expondo horas extras na api de funcionarios

--- apps/funcionarios/api/serializers.py
+++ apps/funcionarios/api/serializers.py
@@ -1,8 +1,11 @@
 from apps.funcionarios.models import Funcionario
 from rest_framework import serializers
+from apps.registro_hora_extra.api.serializers import RegistroHoraExtraSerializer
 
 
 class FuncionarioSerializer(serializers.ModelSerializer):
+
+    registrohoraextra_set = RegistroHoraExtraSerializer(many=True)
     class Meta:
         model = Funcionario
-        fields = ['nome', 'departamentos', 'empresa', 'user', 'imagem']
+        fields = ['id', 'nome', 'departamentos', 'empresa', 'user', 'imagem', 'total_horas_extras', 'registrohoraextra_set']
